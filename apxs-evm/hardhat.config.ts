a4b3799c2f7df592c107463e229be4d36aa6eaf2
Dhruv Kathuria <apraxusblockchain@gmail.com>
2026-08-23 11:35:00 +0530

    Add Ethereum mainnet network configuration

--- apxs-evm/hardhat.config.ts
+++ apxs-evm/hardhat.config.ts
@@ -46,5 +46,12 @@ export default defineConfig({
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+
+    mainnet: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("MAINNET_RPC_URL"),
+      accounts: [configVariable("MAINNET_PRIVATE_KEY")],
+    },
   },
 });
